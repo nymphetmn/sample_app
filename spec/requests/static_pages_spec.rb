4e79ffecc22dd660d55eb88144e5fa9c4194fd71
Alexey A. Pashkin <nymphetmn@gmail.com>
2016-08-16 17:29:52 +0300

    Очередной рефакторинг тестов. Добавлена статическая страница Contact.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 
 describe "Static pages" do
 
+  let(:baseTitle) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -12,7 +14,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{baseTitle} | Home")
     end
   end
 
@@ -25,7 +27,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{baseTitle} | Help")
     end
   end
 
@@ -38,7 +40,19 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("About Us")
+      expect(page).to have_title("#{baseTitle} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{baseTitle} | Contact")
     end
   end
 
