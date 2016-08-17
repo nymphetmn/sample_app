c3d2e31bdabef84c6051be6c5f9b74fc1a0f282a
Alexey A. Pashkin <nymphetmn@gmail.com>
2016-08-17 11:52:09 +0300

    Маршруты до статических страниц теперь заданы с помощью config/routes.rb.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,17 +8,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "должна иметь правильный заголовок" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{baseTitle}")
     end
 
     it "не должна иметь в базовом заголовке Home" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title("| Home")
     end
   end
@@ -26,17 +26,17 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "должна иметь правильный заголовок" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{baseTitle}")
     end
 
     it "не должна иметь в базовом заголовке Help" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).not_to have_title("| Help")
     end
   end
@@ -44,34 +44,34 @@ describe "Static pages" do
   describe "Abous page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "должна иметь правильный заголовок" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{baseTitle}")
     end
 
     it "не должна иметь в базовом заголовке About Us" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).not_to have_title("| About Us")
     end
   end
 
   describe "Contact page" do
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "должна иметь правильный заголовок" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{baseTitle}")
     end
 
     it "не должна иметь в базовом заголовке Contact" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).not_to have_title("| Contact")
     end
   end
