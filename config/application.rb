26bee41590863341096d0e43294798b196050e4c
Alexey A. Pashkin <nymphetmn@gmail.com>
2016-08-17 10:37:22 +0300

    Добавлен гем для Bootstrap. Добавлена таблица стилей custom.css.scss, первая версия домашней страницы с приемлимым css-дизайном.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -11,5 +12,6 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
