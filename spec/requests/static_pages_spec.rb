460abe7eee91249f66461642e57c3ba502206a69
Alexey A. Pashkin <nymphetmn@gmail.com>
2016-08-17 12:50:45 +0300

    Рефакторинг тестов с использованием shared_examples.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,50 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe "Static pages" do
+
   subject { page }
   let(:baseTitle) {"Ruby on Rails Tutorial Sample App"}
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(baseTitle) }
+    it { should_not have_title(custom_title) }
+  end
 
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title("#{baseTitle}")}
-    it { should_not have_title ("| Home")}
+    let(:heading)    { 'Sample App' }
+    let(:custom_title) { '| Home' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title("#{baseTitle}")}
-    it { should_not have_title ("| Help")}
+    let(:heading)    { 'Help' }
+    let(:custom_title) { '| Help' }
+
+    it_should_behave_like "all static pages"
+
   end
 
   describe "Abous page" do
     before { visit about_path }
 
-    it { should have_content('About') }
-    it { should have_title("#{baseTitle}")}
-    it { should_not have_title ("| About Us")}
+    let(:heading)    { 'About' }
+    let(:custom_title) { '| About Us' }
 
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title("#{baseTitle}")}
-    it { should_not have_title ("| Contact")}
+    let(:heading)    { 'Contact' }
+    let(:custom_title) { '| Contact' }
+
+    it_should_behave_like "all static pages"
   end
 end
