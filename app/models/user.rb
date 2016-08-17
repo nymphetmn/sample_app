50b61e25fd000f37e2099719dc6aed97ce1164f8
Alexey A. Pashkin <nymphetmn@gmail.com>
2016-08-17 15:19:01 +0300

    Добавлены тесты для валидации адресов email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
- validates(:name, :email, presence: true)
-  validates(:name, length: { maximum: 50 } )
+  validates(:name, length: { maximum: 50 }, presence: true )
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX })
 end
